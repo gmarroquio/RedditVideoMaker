1803d5a8c9f1915f7469a4c86e2f9b28f4636459
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2020-04-23 10:31:16 -0300

    Burrice resolvida

--- criaFillers.sh
+++
@@ -1,6 +0,0 @@
-touch videos/.filler
-touch scripts/.filler
-touch dist/audio/.filler
-touch dist/clips/.filler
-touch dist/composition/.filler
-touch dist/images/.filler
---
+++ criaGitkeep.sh
@@ -0,0 +1,6 @@
+touch videos/.gitkeep
+touch scripts/.gitkeep
+touch dist/audio/.gitkeep
+touch dist/clips/.gitkeep
+touch dist/composition/.gitkeep
+touch dist/images/.gitkeep
